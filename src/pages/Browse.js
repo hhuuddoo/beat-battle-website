dd4468c2bba809ea578c5738f1b02b3af37bae2f
Hudson Ballina <n10219641@qut.edu.au>
2021-03-28 17:56:59 +1000

    fixed card styling

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -7,7 +7,18 @@ export default function Browse() {
       <Header />
       <Content>
         <BrowseGrid>
-          <BattleCard title="Hello" status="VOTING" duration="14h" />
+          <BattleCard title="EPIC BATTLE #51" status="ONGOING" duration="14h" />
+          <BattleCard title="MBLM Battle #3" status="VOTING" duration="14h" />
+          <BattleCard
+            title="DECAP Challenge 8"
+            status="VOTING"
+            duration="14h"
+          />
+          <BattleCard
+            title="COMMUNITY BATTLE #34 | $20 prize for 1st place"
+            status="VOTING"
+            duration="14h"
+          />
         </BrowseGrid>
       </Content>
     </>
