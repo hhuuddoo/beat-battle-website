532e6d6b7dcfb4c52a747f514f4626a7d9d583a2
Hudson Ballina <n10219641@qut.edu.au>
2021-04-03 17:44:33 +1000

    fix height issue on soundcloud player

--- src/components/soundcloud-player.js
+++ src/components/soundcloud-player.js
@@ -2,24 +2,21 @@ import React from "react";
 
 export default function SoundcloudPlayer({
   width = "100%",
-  height = "450px",
+  height = "100%",
   url,
   autoPlay = false,
   hideRelated = false,
   showComments = false,
   showUser = true,
   showReposts = false,
-  visual = true,
   color = "ff5500",
   ...restProps
 }) {
-  let src = visual
-    ? `https://w.soundcloud.com/player/?url=${url}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}&amp;visual=${visual}`
-    : `https://w.soundcloud.com/player/?url=${url}&amp;color=${color}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}`;
+  let src = `https://w.soundcloud.com/player/?url=${url}&amp;color=${color}&amp;auto_play=${autoPlay}&amp;hide_related=${hideRelated}&amp;show_comments=${showComments}&amp;show_user=${showUser}&amp;show_reposts=${showReposts}`;
   return (
     <iframe
       width={width}
-      height={visual ? height : "auto"}
+      height={height}
       scrolling="no"
       frameborder="no"
       src={src}
