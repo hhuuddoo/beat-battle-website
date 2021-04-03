430aaa017fc12832dc9ac8d66624c6086dad8329
Hudson Ballina <n10219641@qut.edu.au>
2021-04-03 22:59:57 +1000

    setup Browse page to utilise test data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       {/* <Home /> */}
-      {/* <Browse /> */}
+      <Browse />
       {/* <Create /> */}
       <Battle />
     </>
--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowseGrid, Content, Header, BattleCard } from "../components";
+import data from "../testFirstbaseData.json";
 
 export default function Browse() {
+  const [battles, setBattles] = useState([]);
+
+  useEffect(() => {
+    setBattles(data.battles);
+  }, []);
+
   return (
     <>
       <Header />
       <Content>
         <BrowseGrid>
-          <BattleCard title="EPIC BATTLE #51" status="ONGOING" duration="14h" />
+          {battles.map((battle, idx) => (
+            <BattleCard
+              key={idx}
+              title={battle.title}
+              status={battle.votingCloseTime}
+              duration={battle.submissionCloseTime}
+              onClick={() => console.log("Hello")}
+            />
+          ))}
+          {/* <BattleCard title="EPIC BATTLE #51" status="ONGOING" duration="14h" />
           <BattleCard title="MBLM Battle #3" status="VOTING" duration="14h" />
           <BattleCard
             title="DECAP Challenge 8"
@@ -18,7 +34,7 @@ export default function Browse() {
             title="COMMUNITY BATTLE #34 | $20 prize for 1st place"
             status="VOTING"
             duration="14h"
-          />
+          /> */}
         </BrowseGrid>
       </Content>
     </>
