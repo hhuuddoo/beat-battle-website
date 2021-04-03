da9f6d39e950da4f72cd5721a7868b06d8d77c87
Hudson Ballina <n10219641@qut.edu.au>
2021-04-03 14:00:29 +1000

    added modal open and close functionality

--- src/components/submit-modal.js
+++ src/components/submit-modal.js
@@ -1,28 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { BattleContext } from "../context/battle-context";
 
 export default function SubmitModal() {
   const [url, setUrl] = useState("");
+  const { setModalOpen } = useContext(BattleContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     alert("Beat Submitted");
+    // CLOSE MODAL
   };
 
   return (
-    <div className="submit-modal modal">
-      <form className="modal__form" onSubmit={handleSubmit}>
-        <h1 className="modal__form--title">Submit Beat</h1>
-        <input
-          type="url"
-          placeholder="https://www.youtube.com or https://www.dropbox.com"
-          pattern="https://.+"
-          required
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          className="modal__form--input"
-        />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <>
+      <div className="overlay" onClick={() => setModalOpen(false)}></div>
+      <div className="submit-modal modal">
+        <form className="modal__form" onSubmit={handleSubmit}>
+          <h1 className="modal__form--title">Submit Beat</h1>
+          <input
+            type="url"
+            placeholder="https://www.youtube.com or https://www.dropbox.com"
+            pattern="https://.+"
+            required
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            className="modal__form--input"
+          />
+          <input type="submit" value="Submit" className="modal__form--submit" />
+        </form>
+      </div>
+    </>
   );
 }
--- src/pages/Battle.js
+++ src/pages/Battle.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BattleGrid,
   SubmissionCard,
@@ -6,8 +6,11 @@ import {
   Header,
   SubmitModal,
 } from "../components";
+import { BattleContext } from "../context/battle-context";
 
 export default function Battle() {
+  const [modalOpen, setModalOpen] = useState(false);
+
   // Redirect user to website where sample or samples are being hosted
   const handleSampleClick = () => {
     alert(
@@ -22,29 +25,31 @@ export default function Battle() {
 
   return (
     <>
-      <Header />
-      <Content>
-        <SubmitModal />
-        <BattleGrid>
-          <h2 className="battle-title">Title</h2>
-          <span className="samples-button" onClick={handleSampleClick}>
-            Samples
-          </span>
-          <span className="submit-button" onClick={handleSubmitPress}>
-            Submit
-          </span>
-          <div className="battle-description">
-            <p>
-              to nobis sunt repellat quaerat facilis qui tempore aperiam sit
-              maiores! Ea, voluptates.
-            </p>
-          </div>
-          <h3>Submissions</h3>
-          <SubmissionCard>1</SubmissionCard>
-          <SubmissionCard>2</SubmissionCard>
-          <SubmissionCard>3</SubmissionCard>
-        </BattleGrid>
-      </Content>
+      <BattleContext.Provider value={{ setModalOpen }}>
+        <Header />
+        <Content>
+          {modalOpen && <SubmitModal />}
+          <BattleGrid>
+            <h2 className="battle-title">Title</h2>
+            <span className="samples-button" onClick={handleSampleClick}>
+              Samples
+            </span>
+            <span className="submit-button" onClick={() => setModalOpen(true)}>
+              Submit
+            </span>
+            <div className="battle-description">
+              <p>
+                to nobis sunt repellat quaerat facilis qui tempore aperiam sit
+                maiores! Ea, voluptates.
+              </p>
+            </div>
+            <h3>Submissions</h3>
+            <SubmissionCard>1</SubmissionCard>
+            <SubmissionCard>2</SubmissionCard>
+            <SubmissionCard>3</SubmissionCard>
+          </BattleGrid>
+        </Content>
+      </BattleContext.Provider>
     </>
   );
 }
