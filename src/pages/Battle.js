b378164efeb2f7b302f064d9b50d1aa18787d534
Hudson Ballina <n10219641@qut.edu.au>
2021-04-03 12:49:25 +1000

    add battle grid styling

--- src/pages/Battle.js
+++ src/pages/Battle.js
@@ -7,24 +7,15 @@ export default function Battle() {
       <Header />
       <Content>
         <BattleGrid>
-          <h2>Title</h2>
-          <button>Hi</button>
-          <button>Bye</button>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione,
-            quaerat alias voluptatibus quam aspernatur eveniet repellat
-            praesentium eos asperiores pariatur earum temporibus fugit quos
-            officiis facere ex molestiae, quisquam nostrum exercitationem
-            repellendus blanditiis aliquam iste, assumenda architecto? Maxime
-            recusandae vitae omnis at soluta vero ullam dignissimos dolorem
-            accusamus quo molestias maiores minima quam ea molestiae debitis
-            velit, temporibus corrupti eveniet. Minus maxime tempora nesciunt
-            eligendi aperiam ullam doloremque quis optio accusantium animi quo,
-            explicabo maiores nostrum magni ratione deserunt praesentium! Vitae
-            accusantium numquam vel debitis quo autem culpa! Rem necessitatibus
-            sapiente error in ad consequatur natus officia numquam nesciunt
-            dolores!
-          </p>
+          <h2 className="battle-title">Title</h2>
+          <span className="samples-button">Samples</span>
+          <span className="submit-button">Submit</span>
+          <div className="battle-description">
+            <p>
+              to nobis sunt repellat quaerat facilis qui tempore aperiam sit
+              maiores! Ea, voluptates.
+            </p>
+          </div>
           <h3>Submissions</h3>
           <SubmissionCard>1</SubmissionCard>
           <SubmissionCard>2</SubmissionCard>
