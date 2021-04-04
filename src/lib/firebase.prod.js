3eeb035b3cb93b2e6ebda428527dd9023cdf0e35
Hudson Ballina <n10219641@qut.edu.au>
2021-04-04 14:45:44 +1000

    move firebase config into env folder

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "beat-battle-website.firebaseapp.com",
-  databaseURL: "https://beat-battle-website-default-rtdb.firebaseio.com",
-  projectId: "beat-battle-website",
-  storageBucket: "beat-battle-website.appspot.com",
-  messagingSenderId: "336678364850",
-  appId: "1:336678364850:web:f2ef1a95bc33362f658bab",
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebase = Firebase.initializeApp(firebaseConfig);
